bc7ab2de850b7fd543e9de6b508b30e26e57ce8c
bakytdeveloper <bakytdeveloper@gmail.com>
2025-12-03 22:04:08 +0600

    Поправил импорты в компоненте Catalog.jsx

--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Container, Row, Col, Form, Button, Navbar, Nav } from 'react-bootstrap';
