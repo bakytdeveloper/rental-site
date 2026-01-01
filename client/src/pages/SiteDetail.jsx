dbf209c37430832a53fb5e0dec015decbb7ca0ea
bakytdeveloper <bakytdeveloper@gmail.com>
2026-01-01 11:00:47 +0600

    Просто выравниваю SiteDetail.jsx фаил

--- client/src/pages/SiteDetail.jsx
+++ client/src/pages/SiteDetail.jsx
@@ -603,6 +603,7 @@ const SiteDetail = () => {
 
 
 
+
 // Компонент похожих сайтов
 const RelatedSites = ({ currentSiteId, category }) => {
     const [relatedSites, setRelatedSites] = useState([]);
